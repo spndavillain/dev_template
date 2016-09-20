2dea81a431c51f358065629939fb3b27077a3a8b
spndavillain <spndavillain@hotmail.com>
2016-09-20 15:42:25 +0100

    Reverted back to original project

---
+++ application/views/include/navbar_v.php
@@ -0,0 +1,19 @@
+
+<nav class="navbar navbar-default size-off-the-navbar" data-spy="affix" data-offset-top="850">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse " id="myNavbar">
+            <ul class="nav navbar-nav main-menu-links">
+                <li><a href="#">SOBRE NÓS</a></li>
+                <li><a href="#">CARTÃO CORAÇÃO</a></li>
+                <li><a href="#">CONTACTOS</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
