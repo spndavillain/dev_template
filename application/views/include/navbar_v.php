00703c11ac78729b9c0918233e5c15b0dd7eed51
spndavillain <spndavillain@hotmail.com>
2016-09-22 15:26:20 +0100

    Solve The navbar Issue

--- application/views/include/navbar_v.php
+++ application/views/include/navbar_v.php
@@ -1,18 +1,21 @@
-<nav class="navbar navbar-default size-off-the-navbar" data-spy="affix" data-offset-top="850">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+
+    <nav class="navbar navbar-default size-off-the-navbar" data-spy="affix" data-offset-top="850">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse " id="myNavbar">
+                <ul class="nav navbar-nav main-menu-links">
+                    <li><a href="#">SOBRE NÓS</a></li>
+                    <li><a href="#">CARTÃO CORAÇÃO</a></li>
+                    <li><a href="#">CONTACTOS</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="collapse navbar-collapse " id="myNavbar">
-            <ul class="nav navbar-nav main-menu-links">
-                <li><a href="#">SOBRE NÓS</a></li>
-                <li><a href="#">CARTÃO CORAÇÃO</a></li>
-                <li><a href="#">CONTACTOS</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+    </nav>
+
+
